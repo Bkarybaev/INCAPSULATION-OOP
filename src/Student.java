92fbcc787518a63b0da740e0a75dc42ee42755b1
Bkarybaev <baielkarybaev@gmail.com>
2024-11-06 20:08:09 +0600

    first commit

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
-
 public class Student {
+    private String name;
+    private String surname;
+    private int age;
+    String[] course;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getSurname(){
+        return surname;
+    }
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+    public  int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public String[] getCourse(){
+        return course;
+    }
+    public void setCourse(String[] course){
+        this.course = course;
+    }
 }
